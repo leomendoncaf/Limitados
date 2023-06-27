e9881ec9029226bab7e9958d25ea33c452f3cf6d
joaodutra88 <jvitordutra@gmail.com>
2023-06-27 18:33:19 -0300

    att heroku

--- limitados-app/src/config/app.config.ts
+++ limitados-app/src/config/app.config.ts
@@ -1,5 +1,7 @@
 export default function useConfig() {
-  const apiBaseUrl = process.env["API_BASE_URL"] ?? "http://127.0.0.1:3030/api";
+  const apiBaseUrl =
+    process.env["API_BASE_URL"] ??
+    "https://limitados-c50f4155939c.herokuapp.com/api";
 
   return {
     apiBaseUrl,
